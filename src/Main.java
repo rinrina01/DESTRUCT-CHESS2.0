2c325c893b1fe423b16e45ee676b9f0d24d22401
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-02-07 21:22:28 +0100

    finished easterEgg

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import Controller.Gamebase;
 import Vue.Cli;
 
 public class Main {
     public static void main(String[] args) {
-        Gamebase.easterEgg();
-        // while (true) {
-        //     Cli.openMenu(0); // opens the menu
-        // }
+        while (true) {
+            Cli.openMenu(0); // opens the menu
+        }
     }
 }
